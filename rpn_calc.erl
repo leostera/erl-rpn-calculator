b54e9f95e71f313c3d9ce1a70a4c58def64ff68a
Leandro Ostera <me@leostera.com>
2015-03-12 19:53:34 -0300

    adds basic operations tests

--- rpn_calc.erl
+++ rpn_calc.erl
@@ -6,6 +6,12 @@
 
 test() ->
     5 = rpn("5"),
+    87 = rpn("90 3 -"),
+    93 = rpn("90 3 +"),
+    20 = rpn("10 2 *"),
+    5 = rpn("10 2 /"),
+    8 = rpn("2 3 ^"),
+    true = math:sqrt(2) == rpn("2 0.5 ^"),
     ok.
 
 rpn(L) when is_list(L) ->
